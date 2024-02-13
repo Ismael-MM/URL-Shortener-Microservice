18e4a5c9edb11999d5c58517fa6b06f6ecf6ccfd
ismael <ismontelongo@outlook.com>
2024-02-13 16:27:47 +0000

    1.2.1

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv');
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
 
 // Basic Configuration
